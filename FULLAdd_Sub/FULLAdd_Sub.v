c9c06cf2422f1359830c2f59e2336692f03cc993
Amin-Vossoughian <m.a.vossoughian+2@hotmail.com>
2023-02-13 14:38:28 +0100

    Full Adder subtractor improved and error removed and simulated

--- FULLAdd_Sub/FULLAdd_Sub.v
+++ FULLAdd_Sub/FULLAdd_Sub.v
@@ -1,47 +1,12 @@
-module FAdd_sub(input [3:0] a_in, b_in, input control_in, output [3:0] sum_out , carry_out);
+module FAdd_sub(input [3:0] a_in, b_in, input control_in, output [3:0] sum_out ,output  carry_out);
 
 reg [4:0] temp_result;
 
 assign {carry_out, sum_out} = a_in + temp_result;
 
 always@ * 
-  begin 
-  if (~control_in){
-  temp_result = b_in + control_in; 
-  } else {
-  temp_result = ~b_in + control_in;
-  }
-  end
-endmodule
+ 
+  if (~control_in)  temp_result = b_in + control_in; 
+  else  temp_result = (~b_in) + control_in;
 
-module FAdd_sub_tb;
-
-reg [3:0] a_in, b_in;
-reg control_in;
-wire [3:0] sum_out, carry_out;
-
-FAdd_sub   DUT(.a_in(a_in), .b_in(b_in), .control_in(control_in), .sum_out(sum_out), .carry_out(carry_out));
-
-always@ *
-initial
-
-$monitor ("time:%d , a_in=%b\tb_in=%b\tcontrol_in=%b\t,sum_out=%b\t,carry_out=%b\t", a_in, b_in, control_in, sum_out, carry_out);
-control_in=1b0; b_in=4b0000; a_in=4b0000;
-#10 control_in=1b0; b_in=4b0001; a_in=4b0001;
-#10 control_in=1b0; b_in=4b0010; a_in=4b0001;
-#10 control_in=1b0; b_in=4b0100; a_in=4b0001;
-#10 control_in=1b0; b_in=4b1000; a_in=4b0001;
-#10 control_in=1b0; b_in=4b1100; a_in=4b0011;
-#10 control_in=1b0; b_in=4b1110; a_in=4b0111;
-#10 control_in=1b1; b_in=4b1111; a_in=4b1111;
-#10 control_in=1b1; b_in=4b0001; a_in=4b0001;
-#10 control_in=1b1; b_in=4b0010; a_in=4b0001;
-#10 control_in=1b1; b_in=4b0100; a_in=4b0001;
-#10 control_in=1b1; b_in=4b1000; a_in=4b0001;
-#10 control_in=1b1; b_in=4b1100; a_in=4b0011;
-#10 control_in=1b1; b_in=4b1110; a_in=4b0111;
-#10 control_in=1b1; b_in=4b1111; a_in=4b1111;
-# $stop;
-
-end
 endmodule
---
+++ FULLAdd_Sub/FULLAdd_Sub_tb.v
@@ -0,0 +1,33 @@
+module FAdd_sub_tb;
+
+reg [3:0] a_out, b_out;
+reg control_out;
+wire [3:0] sum_in;
+wire carry_in;
+
+FAdd_sub   DUT(.a_in(a_out), .b_in(b_out), .control_in(control_out), .sum_out(sum_in), .carry_out(carry_in));
+
+initial
+begin
+
+$monitor ("time:%d \t, a_in=%d \t b_in=%d \t, control_in=%b \t, sum_out=%d \t, carry_out=%b", $time, a_out, b_out, control_out, sum_in, carry_in);
+
+control_out=1'b0; b_out=4'b0000; a_out=4'b0000;
+#10 control_out=1'b0; b_out=4'b0001; a_out=4'b0001;
+#10 control_out=1'b0; b_out=4'b0010; a_out=4'b0001;
+#10 control_out=1'b0; b_out=4'b0100; a_out=4'b0001;
+#10 control_out=1'b0; b_out=4'b1000; a_out=4'b0001;
+#10 control_out=1'b0; b_out=4'b1100; a_out=4'b0011;
+#10 control_out=1'b0; b_out=4'b1110; a_out=4'b0111;
+#10 control_out=1'b1; b_out=4'b1111; a_out=4'b1111;
+#10 control_out=1'b1; b_out=4'b0001; a_out=4'b0001;
+#10 control_out=1'b1; b_out=4'b0010; a_out=4'b0001;
+#10 control_out=1'b1; b_out=4'b0100; a_out=4'b0001;
+#10 control_out=1'b1; b_out=4'b1000; a_out=4'b0001;
+#10 control_out=1'b1; b_out=4'b1100; a_out=4'b0011;
+#10 control_out=1'b1; b_out=4'b1110; a_out=4'b0111;
+#10 control_out=1'b1; b_out=4'b1111; a_out=4'b1111;
+#10 $stop;
+
+end
+endmodule
