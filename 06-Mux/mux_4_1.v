0b69377bd66730718a5362eec7ce65371da803dd
Amin-Vossoughian <m.a.vossoughian+2@hotmail.com>
2023-02-14 12:34:16 +0100

    All the muxes are compile and errors are addressed

--- 06-Mux/mux_4_1.v
+++ 06-Mux/mux_4_1.v
@@ -7,16 +7,16 @@ module mux_4_1(y_out, d_in, sel_in);
 
 input  [3:0] d_in;
 input  [1:0] sel_in;
-output [3:0] y_out;
+output reg y_out;
 
-always@*
-begin 
+always@*  begin 
 	case(sel_in)
-	2'b00 : y_out=d_in[0];
-	2'b01 : y_out=d_in[1];
-	2'b10 : y_out=d_in[2];
-	2'b11 : y_out=d_in[3];
-	end case
+		2'b00 :  y_out = d_in[0];
+		2'b01 :  y_out = d_in[1];
+		2'b10 :  y_out = d_in[2];
+		2'b11 :  y_out = d_in[3];
+		default: y_out = 0;
+	endcase
 end
 endmodule
 
--- 06-Mux/mux_4_1_2i1o.v
+++ 06-Mux/mux_4_1_2i1o.v
@@ -6,13 +6,13 @@ module mux_4_1_2i1o (y_out, d_in, sel_in);
 
 input [3:0] d_in;
 input [1:0] sel_in;
-output [3:0] y_out;
+output y_out;
 
 reg temp_1 , temp_2;
 
 always@*
 begin
-	case (sel_in[0]) begin
+	case (sel_in[0])
 		1'b0 : begin
 			temp_1=d_in[0];
 			temp_2=d_in[2];
@@ -24,6 +24,6 @@ begin
 	endcase
 end 
 
-assign y_out = (sel_in[1]) ? tem_2 : temp_1;
+assign y_out = (sel_in[1]) ? temp_2 : temp_1;
 
 endmodule 
