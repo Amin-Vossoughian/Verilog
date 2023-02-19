dd50cdef85e9262181ad36b64095c2180be89388
Amin-Vossoughian <m.a.vossoughian+2@hotmail.com>
2023-02-19 19:25:45 +0100

    johnson and ring conter are describe, required to be simulated and synthesize

---
+++ 11-counters/ring_counters/counter_tb.v
@@ -0,0 +1,29 @@
+module counter_tb;
+parameter N=10;
+parameter ring_counter_test = 1;
+reg clk;
+reg reset_al_in;
+wire [N-1:0] count_out;
+
+if (ring_counter_test == 1)
+rign_counter DUT(.clk(clk), .count_out(count_out), .reset_al_in(reset_al_in));
+
+else
+johnson_counter DUT(.clk(clk), .count_out(count_out), .reset_al_in(reset_al_in));
+
+
+always #10 clk = ~clk;
+
+initial begin
+$monitor( "time:%d reset_al_in=%b\t clk=%b\t count_out=%b\t", $time, reset_al_in, clk, count_out);
+
+reset_al_in=1'b0;
+
+#30 reset_al_in=1'b1;
+
+#200 reset_al_in=1'b0;
+
+#50 reset_al_in=1'b1;
+
+end
+endmodule
---
+++ 11-counters/ring_counters/johnson_counter.v
@@ -0,0 +1,14 @@
+module johnson_counter#(parameter N=10)(clk, count_out, reset_al_in);
+
+input clk;
+input reset_al_in;
+output reg [N-1:0] count_out;
+
+always@(posedge clk, negedge reset_al_in)
+
+	if (~reset_al_in)	
+		count_out <= 0;		
+	else
+		count_out <={~count_out[0], count_out[N-1:1]};
+
+endmodule
